4fa1fc81bd6904ce2c2e1dd40a9f2aa56e7d1f14
toms.bergmanis <toms.bergmanis@tilde.lv>
2024-03-27 17:42:26 +0200

    Fixed typo

--- finetune.py
+++ finetune.py
@@ -93,7 +93,7 @@ def main(training_args, hparams):
 
     # Create a new dataset with 'source' and 'target' columns
     dev_set = dev_set_src.map(
-        lambda examples: {'translations': {SRC_LANG_CODE: examples['sentence'],
+        lambda examples: {'translation': {SRC_LANG_CODE: examples['sentence'],
                                            TRG_LANG_CODE: dev_set_trg[examples['id']-1]['sentence']}},
         remove_columns=['id', 'URL', 'domain', 'topic', 'has_image', 'has_hyperlink', 'sentence']
     )
